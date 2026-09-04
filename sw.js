1ee3bf74e840870d2a3485f2fbecddce88273972
casmin7 <cosmin.ciurevici2017@gmail.com>
2026-09-04 14:59:31 +0300

    fixed I hope

--- sw.js
+++ sw.js
@@ -6,7 +6,8 @@ const ASSETS = [
   '/script.js',
   '/favicon.ico',
   '/favicon/favicon.ico',
-  '/favicon/favicon.png',
+  '/favicon/favicon-light-96x96.png',
+  '/favicon/favicon-dark-96x96.png',
   '/favicon/favicon.svg',
   '/favicon/site.webmanifest',
   '/favicon/apple-touch-icon.png'
@@ -14,36 +15,39 @@ const ASSETS = [
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.add(url).catch((err) => {
-                    console.error(`Failed to cache asset: ${url}`, err);
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      // Loop over each asset individually so one failing asset doesn't break everything
+      return Promise.all(
+        ASSETS.map((url) => {
+          return cache.add(url).catch((err) => {
+            console.error(`Failed to cache asset: ${url}`, err);
+          });
+        })
+      );
+    })
   );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys()
-      .then((cacheNames) => {
-        return Promise.all(
-          cacheNames.map((name) => {
-            if (name !== CACHE_NAME) {
-              return caches.delete(name);
-            }
-          })
-        );
-      })
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
